9b2564a23199b8456614fc26d61d0bb57f76b5a1
Abderrazak LAANAYA <laanayabdrzak@gmail.com>
2024-03-03 23:47:49 +0100

    Initial Commit

---
+++ DropDownSwiftUI/DropDownSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropDownSwiftUIApp.swift
+//  DropDownSwiftUI
+//
+//  Created by LAANAYA Abderrazak on 3/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct DropDownSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
